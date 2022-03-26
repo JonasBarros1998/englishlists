df8083f1bb8a7f1b203d1818c18bd3b386c3783a
Jonas Barros <Jonas_barros@outlook.com>
2022-03-26 11:21:24 -0300

    new adapter

---
+++ src/main/java/com/englishclass/englishlists/adapters/DataBase.java
@@ -0,0 +1,9 @@
+package com.englishclass.englishlists.adapters;
+
+import java.util.ArrayList;
+
+import com.englishclass.englishlists.application.model.List;
+
+public interface DataBase {
+  void insert(ArrayList<List> list);
+}
