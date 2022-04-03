cbcdcd4b865f84b62e0a45c9778bc160178796f7
Jonas Barros <Jonas_barros@outlook.com>
2022-04-03 15:18:50 -0300

    create new connection with the mongoDB

--- src/main/java/com/englishclass/englishlist/infra/mongoDB/Connection.java
+++ src/main/java/com/englishclass/englishlist/infra/mongoDB/Connection.java
@@ -1,8 +1,15 @@
 package com.englishclass.englishlist.infra.mongoDB;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Connection {
-  private String connectionString;
+
+  public @Bean MongoClient mongoClient() {
+       return MongoClients.create("mongodb://localhost:27017");
+  }
 }
