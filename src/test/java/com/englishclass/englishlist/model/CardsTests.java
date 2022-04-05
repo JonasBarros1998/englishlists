21f102d7d737b891e41d093bcb4ad9524f8c0261
Jonas Barros <Jonas_barros@outlook.com>
2022-04-05 17:37:18 -0300

    changing unit tests

--- src/test/java/com/englishclass/englishlist/model/CardsTests.java
+++ src/test/java/com/englishclass/englishlist/model/CardsTests.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.ArrayList;
 
 import com.englishclass.englishlist.application.model.Card;
-import com.englishclass.englishlist.application.model.List;
+import com.englishclass.englishlist.application.model.ListOfCards;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,7 +22,7 @@ class CardsTests {
 
 	@Test
 	public void shouldToInsertANewUserInAList() {
-		List list = new List();
+		ListOfCards list = new ListOfCards();
 		list.setTitle("my first list");
 
 		for (Card card : listOfCards) {
