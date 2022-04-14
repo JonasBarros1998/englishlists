9bfef438443b87385b78ebd7ec3ea4c09cf6e1e0
Jonas Barros <Jonas_barros@outlook.com>
2022-04-14 20:42:49 -0300

    creating connection for mongodb

--- src/main/java/com/englishclass/englishlist/infra/mongoDB/Connection.java
+++ src/main/java/com/englishclass/englishlist/infra/mongoDB/Connection.java
@@ -3,17 +3,9 @@ package com.englishclass.englishlist.infra.mongoDB;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
 public class Connection {
 
-  @Value("${englishlists.connectionString}")
-  private String connectionString;
-
-  public @Bean MongoClient mongoClient() {
-       return MongoClients.create(connectionString);
+  public MongoClient mongoClient() {
+    return MongoClients.create("mongodb://0.0.0.0:27017/?maxPoolSize=20");
   }
 }
