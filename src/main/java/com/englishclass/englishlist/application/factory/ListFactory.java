cfbef56019418f02097df1a5bb428eefe6f8873f
Jonas Barros <Jonas_barros@outlook.com>
2022-04-18 22:03:43 -0300

    creating new methods

--- src/main/java/com/englishclass/englishlist/application/factory/ListFactory.java
+++ src/main/java/com/englishclass/englishlist/application/factory/ListFactory.java
@@ -7,6 +7,7 @@ import com.englishclass.englishlist.application.model.ListOfCards;
 public class ListFactory {
 
   private ListOfCards list;
+  private Card card;
 
   public ListFactory() {
     this.list = new ListOfCards();
@@ -17,12 +18,27 @@ public class ListFactory {
     return this;
   }
 
+  public ListFactory addIsPrivate(boolean isPrivate) {
+    this.list.setIsPrivate(isPrivate);
+    return this;
+  }
+
   public ListFactory insertNewCard(Card card) {
     this.list.insertCardInList(card);
     return this;
   }
 
-  public ListFactory create() {
+  public ListFactory addQuantity(int quantity) {
+    this.list.setQuantityOfCards(quantity);
     return this;
+  }
+
+  public ListFactory addCard(String id, String word, String translation, String context) {
+    this.card = new Card(word, translation, context);
+    return this;
+  }
+
+  public ListOfCards create() {
+    return this.list;
   }
 }
