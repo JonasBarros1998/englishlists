b1580cdba958d18200355c459a1dcdddfdf623a2
Jonas Barros <Jonas_barros@outlook.com>
2022-04-18 22:03:03 -0300

    implement the interface DatabaseRepository

---
+++ src/main/java/com/englishclass/englishlist/adapters/output/DatabaseRepositoryImpl.java
@@ -0,0 +1,31 @@
+package com.englishclass.englishlist.adapters.output;
+
+import java.util.ArrayList;
+
+import com.englishclass.englishlist.application.DTO.ListOfCardsDTO;
+import com.englishclass.englishlist.ports.secondary.DatabaseRepository;
+import com.englishclass.englishlist.infra.mongoDB.Find;
+
+public class DatabaseRepositoryImpl implements DatabaseRepository  {
+
+  private Find find; 
+
+  public DatabaseRepositoryImpl() {
+    this.find = new Find();
+  }
+
+  @Override
+  public void insert(ListOfCardsDTO listOfCardsDTO) {}
+
+  @Override
+  public ArrayList<ListOfCardsDTO> find(int limit) {
+    ArrayList<ListOfCardsDTO> listOfCardsDTO = this.find.document(limit);
+    return listOfCardsDTO;
+  }
+
+  @Override
+  public ArrayList<ListOfCardsDTO> find() {
+    ArrayList<ListOfCardsDTO> listOfCardsDTO = this.find.document();
+    return listOfCardsDTO;
+  }
+}
