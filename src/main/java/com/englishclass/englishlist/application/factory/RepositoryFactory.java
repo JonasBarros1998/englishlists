278f056808f34e28f176424746e24ca65111ddf9
Jonas Barros <Jonas_barros@outlook.com>
2022-04-23 22:35:15 -0300

    implementing new method

--- src/main/java/com/englishclass/englishlist/application/factory/RepositoryFactory.java
+++ src/main/java/com/englishclass/englishlist/application/factory/RepositoryFactory.java
@@ -2,10 +2,16 @@ package com.englishclass.englishlist.application.factory;
 
 import com.englishclass.englishlist.adapters.output.DatabaseRepositoryImpl;
 import com.englishclass.englishlist.application.useCase.FindListOfCardsOnDatabase;
+import com.englishclass.englishlist.application.useCase.InsertEnglishListOnDatabase;
 
 public class RepositoryFactory {
   
   public FindListOfCardsOnDatabase buildDatabaseRepository() {
     return new FindListOfCardsOnDatabase(new DatabaseRepositoryImpl());
   }
+
+  public InsertEnglishListOnDatabase buildDatabaseRepositoryForInserNewEnglishList() {
+    return new InsertEnglishListOnDatabase(new DatabaseRepositoryImpl());
+  }
+
 }
